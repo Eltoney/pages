2dbe09e86ca90b1df25cd5c1728995de036726e2
Eltoney <aliahmedeltoney@gmail.com>
2023-01-24 13:31:48 +0200

    Update to deploy on herkou

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3ff5puyt38q(kq=ld@4#$%$@&uo(kq*g9d5yutif4ac&qw^x30
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
